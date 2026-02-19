d8bd6ffcd1641193e698b1378bd54ab84647dba7
yoolalw <larakemilyprofissional@gmail.com>
2026-02-18 22:38:34 -0300

    api conectada na home!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
 }
 
 dependencies {
--- app/src/main/java/com/example/bloomskimono/ProductApi.kt
+++ app/src/main/java/com/example/bloomskimono/ProductApi.kt
@@ -1,4 +1,5 @@
 package com.example.bloomskimono
+import com.example.bloomskimono.models.Product
 
 import retrofit2.Call
 import retrofit2.http.GET
